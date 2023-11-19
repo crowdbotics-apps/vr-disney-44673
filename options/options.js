dc24d0295dfdfd977b6b9c214172f15f30a0dca9
Crowdbotics <team@crowdbotics.com>
2023-11-19 20:23:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vr-disney-44673",
+  url: "https://vr-disney-44673.botics.co",
+  api: "https://vr-disney-44673.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
